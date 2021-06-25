00ff1aa234b30f14df5b5f671886d4ddefc25c53
ElStepanyan <ElStepanyan>
2021-06-25 08:30:45 +0000

    fixed bug with annotation import

--- src/main/java/com/example/demo/controller/SimpleController.java
+++ src/main/java/com/example/demo/controller/SimpleController.java
@@ -2,13 +2,14 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.time.LocalDateTime;
 
 @RestController
 public class SimpleController {
 	
-	@Value("myapp.test")
+	@Value("${myapp.test}")
 	private String stringValue;
 
 	@RequestMapping("/current_time")
